378197dc7191d52d4c580121873cc258dbafb7f9
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-03-01 22:13:35 -0300

    Criado componente Post e testado no componente App

--- lambe/App.js
+++ lambe/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
 import Header from './src/components/Header'
+import Post from './src/components/Post'
+import {View, Text} from 'react-native'
+
 export default class App extends Component {
   render() {
     return (
-      <Header />
+      <View style={{flex: 1}}>
+        <Header />
+        <Post image={require('./assets/imgs/fence.jpg')}/>
+      </View>
     )
   }
 }
