eeff3f13c406121cbc48e5dfe1f6dd858041bdb9
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-03-02 08:07:21 -0300

    Adicionados componentes: Autor, Comments, AddComment

--- lambe/App.js
+++ lambe/App.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Header from './src/components/Header'
 import Post from './src/components/Post'
-import {View, Text} from 'react-native'
+import { View } from 'react-native'
 
 export default class App extends Component {
   render() {
+    const comments = [{
+      nickname: 'Joana Elena Silva',
+      comment: 'Excelente Foto!'
+    },
+    {
+      nickname: 'Rafael dos Santos',
+      comment: 'Nossa que LIXO!!!'
+    }
+    ]
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')}/>
+        <Post image={require('./assets/imgs/fence.jpg')}
+          comments={comments} />
       </View>
     )
   }
--- lambe/src/components/Post.js
+++ lambe/src/components/Post.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { View, Image, StyleSheet, Dimensions } from 'react-native'
+import Autor from './Autor'
+import Comments from './Comments'
+import AddComment from './AddComment'
 
 class Post extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Image source={this.props.image} style={styles.image}/>
+        <Autor email="gustavoksilva3@hotmail.com" nickname="Fulano_de_tal"/>
+        <Comments comments={this.props.comments}/>
+        <AddComment />
       </View>
     )
   }
